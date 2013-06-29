6141e92cff685397725244a9ad6ce5e91a7ec43c
Leonardo Wistuba <leonardowistuba@gmail.com>
2013-06-29 10:38:39 -0300

    Constructors

--- redes2-python/client.py
+++ redes2-python/client.py
@@ -1,9 +1,15 @@
 # coding=UTF-8
 
 from datetime import datetime
+import socket
 
 class Client:
     loggers = []
+    
+    def __init__(self, UDP_IP, UDP_port):
+        self.UDP_IP = UDP_IP
+        self.UDP_port = UDP_port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
     def add_logger(self, logger):
         self.loggers.append(logger)
@@ -16,7 +22,5 @@ class Client:
     def log(self, message):
         self.notify_all(message)
     
-        
-
-    
-    
+    def send(self, message):
+        self.socket.sendto(message, (self.UDP_IP, self.UDP_port))
--- redes2-python/server.py
+++ redes2-python/server.py
@@ -1,1 +1,32 @@
 # coding=UTF-8
+
+import socket
+from datetime import datetime
+
+class Server:
+    loggers = []
+    
+    def __init__(self, UDP_IP, UDP_port):
+        self.UDP_IP = UDP_IP
+        self.UDP_port = UDP_port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind((self.UDP_IP, self.UDP_port))
+    
+    def add_logger(self, logger):
+        self.loggers.append(logger)
+    
+    def notify_all(self, message):
+        time = datetime.time(datetime.now())
+        for logger in self.loggers:
+            logger.log(time, message)
+    
+    def log(self, message):
+        self.notify_all(message)
+        
+    def listen(self):
+        while True:
+            data, addr = self.socket.recvfrom(1024) # buffer size is 1024 bytes
+            print "received message:", data + "with addr: ", addr
+        
+        
+
